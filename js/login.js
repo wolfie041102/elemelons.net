bef29cfe20cc99e4750a096296c3a1b42c64d0b6
Wolfie041102 <km041102@gmail.com>
2021-12-02 19:52:20 +0100

    login fail

--- js/login.js
+++ js/login.js
@@ -1,9 +1,36 @@
-var el = document.getElementById('login');
-if(el){
-  alert("test");
-  el.addEventListener("submit", myfunction);
+const init = function(){
+  var checkExist = setInterval(function() {
+     if ($('#button-login').length) {
+        clearInterval(checkExist);
+        document.getElementById('button-login').addEventListener('click', send);
+     }
+  }, 100)
 }
 
-function myFunction() {
-  alert("The form was submitted");
+const send = function(ev) {
+  ev.preventDefault();
+  ev.stopPropagation();
+
+  let ret = validate();
+
+  if (ret) {
+    console.log('send!!');
+  }
 }
+
+const validate = function(ev) {
+  let username = document.getElementById('username').value;
+  let password = document.getElementById('password').value;
+
+  $.ajax({
+    type: "POST",
+    url: "/python/sensors.py",
+    headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}, //Add this <----
+    success:function(result){
+      console.log(result);
+    },
+  });
+  return true;
+};
+
+document.addEventListener('DOMContentLoaded', init);
---
+++ js/nodelogin.js
@@ -0,0 +1,12 @@
+// temperature-listener.js
+
+const { spawn } = require('child_process');
+const temperatures = []; // Store readings
+
+const sensor = spawn('python', ['sensor.py']);
+sensor.stdout.on('data', function(data) {
+
+    // convert Buffer object to Float
+    temperatures.push(parseFloat(data));
+    console.log(temperatures);
+});
